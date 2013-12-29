1cff330f69e734f7ab583f390a857bd2aadf236b
Thinker K.F. Li <thinker@codemud.net>
2013-12-29 21:54:24 +0800

    C++ utils

---
+++ uncallutils-cxx.h
@@ -0,0 +1,99 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+#ifndef __UNCALLUTILS_CXX_H_
+#define __UNCALLUTILS_CXX_H_
+
+#include "uncallutils.h"
+
+/**
+ * To collect call flows of assigning to the variable.
+ *
+ * Example:
+ *   UNCALL_VARW<int> foo;
+ *
+ * Any assignments to foo would be collected of call flows.
+ */
+template <class T>
+class UNCALL_VARW {
+    T mValue;
+public:
+    UNCALL_VARW() {}
+    UNCALL_VARW(T &aValue) {
+        mValue = aValue;
+    }
+
+    UNCALL_VARW &operator =(const T &aValue) {
+        UNCALL();
+        mValue = aValue;
+    }
+
+    UNCALL_VARW &operator =(UNCALL_VARW &aOther) {
+        UNCALL();
+        mValue = aOther.mValue;
+    }
+
+    operator T() const {
+        return mValue;
+    }
+};
+
+/**
+ * To collect call flows of reading value to the variable.
+ *
+ * Example:
+ *   UNCALL_VARW<int> foo;
+ *
+ * Any reading of foo would be collected of call flows.
+ */
+template <class T>
+class UNCALL_VARR {
+    T mValue;
+public:
+    UNCALL_VARR() {}
+    UNCALL_VARR(T &aValue) {
+        mValue = aValue;
+    }
+
+    UNCALL_VARR &operator =(const T &aValue) {
+        mValue = aValue;
+    }
+
+    UNCALL_VARR &operator =(UNCALL_VARR &aOther) {
+        mValue = aOther.mValue;
+    }
+
+    operator T() const {
+        UNCALL();
+        return mValue;
+    }
+};
+
+/**
+ * To collect call flows of reading and writing value to the variable.
+ */
+template <class T>
+class UNCALL_VARRW {
+    T mValue;
+public:
+    UNCALL_VARRW() {}
+    UNCALL_VARRW(T &aValue) {
+        mValue = aValue;
+    }
+
+    UNCALL_VARRW &operator =(const T &aValue) {
+        UNCALL();
+        mValue = aValue;
+    }
+
+    UNCALL_VARRW &operator =(UNCALL_VARRW &aOther) {
+        UNCALL();
+        mValue = aOther.mValue;
+    }
+
+    operator T() const {
+        UNCALL();
+        return mValue;
+    }
+};
+
+#endif /* __UNCALLUTILS_CXX_H_ */
+
--- uncallutils.h
+++ uncallutils.h
@@ -2,20 +2,28 @@
 #ifndef __UNCALLUTILS_H_
 #define __UNCALLUTILS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct uncall_context uncall_context_t;
 
 extern void uncall(uncall_context_t *);
 extern uncall_context_t *_uncallutils_context;
 extern int _uncallutils_existing;
 
+extern void _uncallutils_init();
+extern void _uncallutils_deinit();
+
+#ifdef __cplusplus
+}
+#endif
+
 #define UNCALL()                      \
     if (_uncallutils_existing) {      \
         uncall(_uncallutils_context); \
     }
 
-
-extern void _uncallutils_init();
-extern void _uncallutils_deinit();
 
 #define UNCALL_INIT() \
     if (_uncallutils_existing) { _uncallutils_init(); }
