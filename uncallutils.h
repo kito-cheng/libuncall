89527f33a5a08ce208bed40a9bcdc9e49fe3a5ab
Thinker K.F. Li <thinker@codemud.net>
2013-12-31 01:24:52 +0800

    Explicit export symbols with default visibility.

--- uncallutils.h
+++ uncallutils.h
@@ -2,25 +2,29 @@
 #ifndef __UNCALLUTILS_H_
 #define __UNCALLUTILS_H_
 
+#ifndef UNC_EXPORT
+#define UNC_EXPORT __attribute__((visibility("default")))
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct uncall_context uncall_context_t;
 
-extern void uncall(uncall_context_t *);
-extern uncall_context_t *_uncallutils_context;
-extern int _uncallutils_existing;
+extern void uncall(uncall_context_t *) UNC_EXPORT;
+extern uncall_context_t * _uncallutils_context UNC_EXPORT;
+extern int _uncallutils_existing UNC_EXPORT;
 
-extern void _uncallutils_init();
-extern void _uncallutils_deinit();
+extern void _uncallutils_init() UNC_EXPORT;
+extern void _uncallutils_deinit() UNC_EXPORT;
 
 #ifdef __cplusplus
 }
 #endif
 
-#define UNCALL()                      \
-    if (_uncallutils_existing) {      \
+#define UNCALL()                        \
+    if (_uncallutils_existing) {        \
         uncall(_uncallutils_context); \
     }
 
